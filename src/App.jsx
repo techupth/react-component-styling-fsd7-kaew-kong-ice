88d021e41e9258f46af59c6ebff46f6a96e8b41d
Kong Thiraphat <thiraphat.cto@gmail.com>
2024-10-22 12:52:11 +0700

    feat: add answer to ex1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
       <div className="button-components-section">
-        {/* Render ตัว Button 2 แบบ */}
+        <Button type="primary" />
+        <Button />
       </div>
       <hr />
       <div className="alert-components-section">
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,44 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button({ type }) {
+  return (
+    <div>
+      {type === "primary" ? (
+        <button
+          css={css`
+            width: 171.19px px;
+            height: 50px px;
+            top: 291px;
+            left: 278.92px;
+            padding: 0px 16px 0px 16px;
+            gap: 8px;
+            border-radius: 4px;
+            opacity: 0px;
+            background: #074ee8;
+          `}
+        >
+          Button
+        </button>
+      ) : (
+        <button
+          css={css`
+            width: 171.19px px;
+            height: 50px px;
+            top: 291px;
+            left: 278.92px;
+            padding: 0px 16px 0px 16px;
+            gap: 8px;
+            border-radius: 4px;
+            opacity: 0px;
+            background: #07a4e8;
+          `}
+        >
+          Button
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default Button;
